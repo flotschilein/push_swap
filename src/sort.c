853590738bb0102cd6b80a2d31642151bb488d2d
Florian Braune <fbraune@1-D-19.42heilbronn.de>
2025-05-29 18:39:50 +0200

    opperations and start of k sort

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: fbraune <fbraune@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 16:58:36 by fbraune           #+#    #+#             */
-/*   Updated: 2025/05/29 17:41:52 by fbraune          ###   ########.fr       */
+/*   Updated: 2025/05/29 18:39:41 by fbraune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,59 @@
 
 static void	small_sort(t_stack **stack_a, t_stack **stack_b, int size)
 {
-}
-static void	push_back(t_stack stack_a, t_stack stack_b, int count)
-{
-	if ((stack_b)->index == count - 1)
+	if (size == 2)
 	{
-		pa(stack_a, stack_b);
-		count--;
+		if ((*stack_a)->value > (*stack_a)->next->value)
+			sa(stack_a);
 	}
-	else
-		rrb(stack_b);
+	else if (size == 3)
+	{
+		// TODO: Implement 3 number sort
+	}
+	else if (size == 4 || size == 5)
+	{
+		// TODO: Implement 4-5 number sort
+	}
 }
+
+static void	push_back(t_stack **stack_a, t_stack **stack_b, int count)
+{
+	while (*stack_b)
+	{
+		if ((*stack_b)->index == count - 1)
+		{
+			pa(stack_a, stack_b);
+			count--;
+		}
+		else
+			rrb(stack_b);
+	}
+}
+
 static void	k_sort(t_stack **stack_a, t_stack **stack_b, int size)
 {
 	int	range;
 	int	count;
 
 	count = 0;
-	range = ft_sqrt(size) * 1.5;
-	while (stacksize != 0)
+	range = ft_sqrt(size) * 1.6;
+	while (size != 0)
 	{
 		if ((*stack_a)->index <= count)
 		{
 			pb(stack_a, stack_b);
 			rb(stack_b);
 			count++;
+            size--;
 		}
 		else if ((*stack_a)->index < count + range)
 		{
 			pb(stack_a, stack_b);
 			count++;
+            size--;
 		}
 		else
 			ra(stack_a);
-		stacksize--;
 	}
 	push_back(stack_a, stack_b, count);
 }
